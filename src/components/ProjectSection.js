0ffc79410de994eb7eae8e4c03352da35d63b99c
Harryguci <huyquang0028@gmail.com>
2023-05-28 20:18:16 +0700

    Add show more button for long paragraphs

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -32,6 +33,56 @@ export default function ProjectSection(props) {
     flexDirection: type === "right" ? "row-reverse" : "row",
   };
 
+  const [isShowAll, setShowAll] = useState(false);
+  let tContent = content;
+
+  let contentElem = (
+    <p style={{ lineHeight: "2rem", letterSpacing: "1px" }}>{tContent}</p>
+  );
+
+  if (content.length > 250) {
+    if (isShowAll) {
+      contentElem = (
+        <p style={{ lineHeight: "2rem", letterSpacing: "1px" }}>
+          {content}
+          <button
+            className="d-inline"
+            style={{
+              background: "none",
+              color: "blue",
+              border: "none",
+            }}
+            onClick={() => {
+              setShowAll(!isShowAll);
+            }}
+          >
+            hide
+          </button>
+        </p>
+      );
+    } else {
+      tContent = tContent.substr(0,250) + "...";
+      contentElem = (
+        <p style={{ lineHeight: "2rem", letterSpacing: "1px" }}>
+          {tContent}
+          <button
+            className="d-inline"
+            style={{
+              background: "none",
+              color: "blue",
+              border: "none",
+            }}
+            onClick={() => {
+              setShowAll(!isShowAll);
+            }}
+          >
+            show more
+          </button>
+        </p>
+      );
+    }
+  }
+
   return (
     <Container id={id} className={className}>
       <Row className="" style={rowStyle}>
@@ -47,9 +98,7 @@ export default function ProjectSection(props) {
           <div className="mt-3 mt-md-5">
             <h2 className="title-thin fs-1">{title}</h2>
             <h2 className="title-bold fs-1 mb-3">{h2}</h2>
-            <p style={{ lineHeight: "2rem", letterSpacing: "1px" }}>
-              {content}
-            </p>
+            {contentElem}
             <Row
               className="mt-5"
               style={{
