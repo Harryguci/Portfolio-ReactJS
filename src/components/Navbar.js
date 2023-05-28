0e648c17bf0416e8070164a3b77fb2acbbbd7e6f
Harryguci <huyquang0028@gmail.com>
2023-05-28 15:15:29 +0700

    Added Navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 import logo from "../logo.svg";
 import "../Assets/Styles/SCSS/navbar.scss";
+import { FaBars } from "react-icons/fa";
 
 const nav_items = [
   { content: "Home", href: "/" },
@@ -10,23 +11,27 @@ const nav_items = [
 ];
 
 export default function Navbar() {
-
   const [toggleMenu, setToggleMenu] = useState(null);
   const menubarRef = useRef(null);
 
   const handleToggleMenu = () => {
     if (toggleMenu === null) setToggleMenu(false);
-    else
-      setToggleMenu(!toggleMenu);
+    else setToggleMenu(!toggleMenu);
     console.log(toggleMenu);
-  }
+  };
   return (
     <nav className="container-fluid mt-3 mt-md-5">
       <div className="container-md d-flex">
         <div className="navbar-brand">
           <img src={logo} alt="logo" />
         </div>
-        <ul ref={menubarRef} className={"nav navbar w-100 justify-content-end d-flex " + (toggleMenu === false ? "d-none" : "active")}>
+        <ul
+          ref={menubarRef}
+          className={
+            "nav navbar w-100 justify-content-end d-flex " +
+            (toggleMenu === false ? "" : "active")
+          }
+        >
           {nav_items.map((item) => (
             <li key={item.content} className="navbar-item">
               <a href="/" className="nav-link">
@@ -41,15 +46,19 @@ export default function Navbar() {
           </li>
         </ul>
         <div className="toggle-menu d-flex justify-content-end">
-            <button 
-              type="button" 
-              className="btn btn-outline-primary rounded-0 mt-3"
-              style={{height: "max-content", fontSize: 0.7 + "rem"}}
-              onClick={handleToggleMenu}
-            >
-              Menu
-            </button>
-          </div>
+          <button
+            type="button"
+            className="btn rounded-0 mt-2"
+            style={{
+              height: "max-content",
+              fontSize: 1 +"rem",
+              color: "white",
+            }}
+            onClick={handleToggleMenu}
+          >
+            <FaBars />
+          </button>
+        </div>
       </div>
     </nav>
   );
